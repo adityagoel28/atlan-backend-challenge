24435d8a1423914c18862bc6735acf46f8e17559
adityagoel28 <adityagoel2002@gmail.com>
2023-07-27 12:53:47 +0530

    schema updated

--- app/models.py
+++ app/models.py
@@ -1,20 +1,46 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class Forms(models.Model):
+    # form_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
+    description = models.TextField()
     email = models.CharField(max_length=255, blank=True)
-    created_by = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # created_by = models.CharField(max_length=255, blank=True)
+    # created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    metadata = JSONField()
 
 class Questions(models.Model):
+    QUESTION_TYPES  = [
+        ('text', 'Text'),
+        ('mcq', 'Multiple Choice'),
+    ]
+
     form = models.ForeignKey(Forms, on_delete=models.CASCADE, related_name="questions")
-    text = models.CharField(max_length=255)
-    mandatory = models.BooleanField(default=False)
+    question_text = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=100, choices=QUESTION_TYPES, default='text')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # mandatory = models.BooleanField(default=False)
+    metadata = JSONField()
+class Choice(models.Model):
+    choice_id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Questions, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
 
 class Responses(models.Model):
     form = models.ForeignKey(Forms, on_delete=models.CASCADE, related_name="responses")
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    metadata = JSONField()
 
 class Answers(models.Model):
     response = models.ForeignKey(Responses, on_delete=models.CASCADE, related_name="answers")
     question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    text = models.TextField()
+    answer_text  = models.TextField()
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE, null=True, blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    metadata = JSONField()
--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response as APIResponse
 from app.models import *
 
+from twilio.rest import Client
+import os
+
 # Create your views here.
 def home(request):
     return APIResponse("Home", status=200)
@@ -10,4 +13,16 @@ def home(request):
 class HealthCheck(APIView):
     def get(self, request):
         return APIResponse("Health OK", status=200)
-    
+
+def sms(request):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+    twilio_number = os.environ['TWILIO_NUMBER']
+
+    try:
+        message = client.messages.create(body='Hi', from_ = twilio_number, to='+918887874339')
+        print(message.sid)
+    except Exception as e:
+        print(f"An error occurred while sending the SMS: {str(e)}")
+    return APIResponse("SMS", status=200)
